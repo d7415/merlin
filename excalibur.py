7f809c52d99888160b66dac87a17f0504745a723
Martin Stone <martin@d7415.co.uk>
2017-05-21 12:21:51 +0100

    Add more information to the Ticks Haven's Started message.
    (cherry picked from commit 5ce09ff057fc20b330435276267a389caf126af4)

--- excalibur.py
+++ excalibur.py
@@ -1310,7 +1310,7 @@ def find1man(max_age):
 if __name__ == "__main__":
     tickstart = GameSetup.get("round_start_time")
     if tickstart and int(tickstart) > time.time() + 300:
-        excaliburlog("Ticks start at %s. Come back later.\n\n" % (datetime.datetime.utcfromtimestamp(float(tickstart)).ctime()))
+        excaliburlog("Ticks start at %s (%s). Come back later.\n\n" % (datetime.datetime.fromtimestamp(float(tickstart)).strftime("%c"), datetime.datetime.utcfromtimestamp(float(tickstart)).strftime("%a %d %H:%M:%S UTC")))
         sys.exit()
 
     bots = []
